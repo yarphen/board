0077c187355b8575e391d5f29c26919597d98233
yarphen <leviatan.ka14@gmail.com>
2017-02-23 13:23:44 +0200

    Securing PostService disallowing deletion for non-author

--- board/src/main/java/com/sheremet/simpleboard/models/PostService.java
+++ board/src/main/java/com/sheremet/simpleboard/models/PostService.java
@@ -2,12 +2,15 @@ package com.sheremet.simpleboard.models;
 
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 public interface PostService {
 
 	List<Post> getAllPosts();
 
 	void addPost(Post body);
 	
+	@PreAuthorize ("@postService.getPostById(#id).getAuthor() == authentication.name")
 	void deletePostById(long id);
 	
 	Post getPostById(long id);
