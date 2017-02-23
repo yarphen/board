167c4a1b78f9e318689a461ad70b92db3c821873
yarphen <leviatan.ka14@gmail.com>
2017-02-23 13:21:25 +0200

    Creating authority implementation

---
+++ board/src/main/java/com/sheremet/simpleboard/auth/Authority.java
@@ -0,0 +1,43 @@
+package com.sheremet.simpleboard.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Authority implements GrantedAuthority {
+	
+	private String name;
+
+	public Authority(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getAuthority() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Authority other = (Authority) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+}
